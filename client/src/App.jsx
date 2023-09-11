054d600e67a6946d361ea0fb748a71af953814bf
aslaterm32 <aslaterm32@gmail.com>
2023-09-11 09:46:02 +0100

    fixed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,30 @@
-import { Routes, Route } from "react-router-dom"
-import { HomePage } from "./Pages/export"
-import { NavBar } from "./Components/export"
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import {
+    HomePage,
+    ProfilePage,
+    TimetablePage,
+    LoginPage,
+    RegisterPage,
+    NotFoundPage,
+} from './Pages/export';
+import { NavBar } from './Components/export';
+import './App.css';
 
 function App() {
-
-  return (
-    <>
-    <NavBar />
-    <Routes>
-      <Route index="/" element={<HomePage />}></Route>
-    </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<NavBar />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="timetable" element={<TimetablePage />} />
+                    <Route path="profile" element={<ProfilePage />} />
+                </Route>
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
