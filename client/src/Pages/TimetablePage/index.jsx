0a860679eff927ab0b7ed329d19ce83ae0fee229
smithnj13 <SmithNJ13@hotmail.com>
2023-09-11 14:49:41 +0100

    sync up changes

--- client/src/Pages/TimetablePage/index.jsx
+++ client/src/Pages/TimetablePage/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import {TimeTable} from "../../Components/export"
 
 const TimetablePage = () => {
-    return <div>TimetablePage</div>;
+    return (
+        <>
+            <TimeTable />
+        </>
+    );
 };
 
 export default TimetablePage;
