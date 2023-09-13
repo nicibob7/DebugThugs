36a983762645a5966b1e13b4d7b40a4a4f7bbc29
emptybagelman <samuelmcgin@aol.com>
2023-09-13 14:05:34 +0100

    finally allows timetable to link to next and prev years

--- client/src/Components/TimeTable/index.jsx
+++ client/src/Components/TimeTable/index.jsx
@@ -16,15 +16,19 @@ const TimeTable = () => {
     return timeSlots;
   };
 
-  const [week, setWeek] = useState(0)
-  const [weekInc,setWeekInc] = useState(0)
+  const [weekInc,setWeekInc] = useState(0) // contains inc/dec of week number
 
-  const [weekDates,setWeekDates] = useState([])
+  const [year, setYear] = useState(new Date().getFullYear()) // stores current year
+
+  const [week,setWeek] = useState(Math.ceil(Math.floor(((new Date()) - (new Date(year,0,1))) / (24*60*60*1000)/7)+1)) // initalize week with current week number
+
+  const [weekDates,setWeekDates] = useState([]) // stores dates of week
 
   const [date,setDate] = useState("")
 
   const [cell, setCell] = useState({ day: "", time: "", weekNum:"" });
 
+  const [calls,setCalls] = useState(0)
 
   const handleClick = (day, time, weekNum) => {
     setCell({ day, time, weekNum });
@@ -32,12 +36,31 @@ const TimeTable = () => {
 
   const changeWeek = (incDec) => {
     setWeekInc(incDec)
-    console.log(weekInc)
+  }
+
+  const updateYear = () => {
+    if(week<1){
+      setWeek(52)
+      setYear(year-1)
+    }
+    else if(week>52){
+      setWeek(1)
+      setYear(year+1)
+    }
+  }
+
+  const resolveWeek = () => {
+    console.log(week)
+    if (week%52 == 0 && calls > 0){
+      return 52
+    }else{
+      return week%52
+    }
   }
 
   const getWeek = () => {
+
     const currDate = new Date()
-    const year = new Date().getFullYear()
     const startDate = new Date(year,0,1)
 
     const weekDays = []
@@ -45,16 +68,13 @@ const TimeTable = () => {
     let weekNum = week;
     let newDate;
 
-    if(!week){
-      let days = Math.floor((currDate - startDate) / (24*60*60*1000))
-      weekNum = Math.ceil(days/7)
-    }
-
     weekNum += weekInc;
+    
 
     let date = (1 + (weekNum - 1) * 7)
     for(let i=1;i<8;i++){
-      newDate = new Date(year,0,date+i)
+      newDate = new Date(year,0,date+i) // change year here
+
       weekDays.push(`${newDate.getDate()}/${newDate.getMonth()+1}/${newDate.getFullYear()}`)
     }
 
@@ -66,7 +86,14 @@ const TimeTable = () => {
   useEffect(() => {
     getWeek()
     setWeekInc(0)
+    setCalls(calls+1)
+
   },[weekInc])
+
+  useEffect(() => {
+    updateYear()
+  },[year])
+
 
   return (
     <div id="timetable" data-testid="timetable">
@@ -74,9 +101,9 @@ const TimeTable = () => {
 
         <button id="left" onClick={(() => changeWeek(-1))} >{"<"}</button>
 
-        <p id="week-number">Week {week}</p>
+        <p id="week-number">Week {resolveWeek()}</p>
 
-        <button id="right" onClick={(() => changeWeek(1))} >{">"}</button>
+        <button id="right" onClick={(() => {changeWeek(1)})} >{">"}</button>
 
       </div>
       <Days days={days} week={week} date={date} weekDates={weekDates}/>
