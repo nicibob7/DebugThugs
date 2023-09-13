339f85ed2ecccfeca721439254ee6a7e906f13e6
smithnj13 <SmithNJ13@hotmail.com>
2023-09-13 10:25:42 +0100

    Timetable GO AWAY!

--- client/src/Components/Timetable/index.jsx
+++ client/src/Components/Timetable/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Days, Times, TableContent } from "../export"
+import axios from "axios"
 import "./style.css";
 
 const TimeTable = () => {
@@ -15,19 +16,71 @@ const TimeTable = () => {
     return timeSlots;
   };
 
+  const [week, setWeek] = useState(0)
+  const [weekInc,setWeekInc] = useState(0)
+
+  const [weekDates,setWeekDates] = useState([])
+
+  const [date,setDate] = useState("")
+
   const [cell, setCell] = useState({ day: "", time: "" });
 
-  const handleClick = (day, time) => {
-    setCell({ day, time });
+
+  const handleClick = (day, time, weekNum) => {
+    setCell({ day, time, weekNum });
   };
-  // console.log(cell)
+
+  const changeWeek =(incDec) => {
+    setWeekInc(weekInc + incDec)
+  }
+
+  const getWeek = () => {
+    const currDate = new Date()
+    const year = new Date().getFullYear()
+    const startDate = new Date(year,0,1)
+
+    const weekDays = []
+
+    let weekNum = week;
+    let newDate;
+
+    if(!week){
+      let days = Math.floor((currDate - startDate) / (24*60*60*1000))
+      weekNum = Math.ceil(days/7)
+    }
+
+
+
+    let date = (1 + (weekNum - 1) * 7)
+    for(let i=1;i<8;i++){
+      newDate = new Date(year,0,date+i)
+      weekDays.push(`${newDate.getDate()}/${newDate.getMonth()+1}/${newDate.getFullYear()}`)
+    }
+
+    setWeek(weekNum+weekInc)
+    setWeekDates(weekDays)
+    setDate(`${newDate.getDate()}/${newDate.getMonth()+1}/${newDate.getFullYear()}`)
+  }
+
+  useEffect(() => {
+    getWeek()
+  },[week])
 
   return (
     <div id="timetable" data-testid="timetable">
-      <Days days={days}/>
+      <div className="week-nav">
+
+        <button id="left" onClick={(() => changeWeek(-1))} >{"<"}</button>
+
+        <p id="week-number">Week {week}</p>
+
+        <button id="right" onClick={(() => changeWeek(1))} >{">"}</button>
+
+      </div>
+      <Days days={days} week={week} date={date} weekDates={weekDates}/>
       <div id="table">
         <Times timeSlots={timeSlots} cell={cell} />
-        <TableContent days={days} cell={cell} timeSlots={timeSlots} handleClick={handleClick} />
+        <TableContent days={days} cell={cell} weekNum={week} timeSlots={timeSlots} handleClick={handleClick} />
       </div>
     </div>
   );
