ee5fc72c0a350ceae69ba2161806487a48d6196e
marcus-am <marcus_mm@outlook.com>
2023-09-14 15:45:30 +0100

    added backend to delete tokens from db

--- client/src/Components/NavBar/index.jsx
+++ client/src/Components/NavBar/index.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import './style.css';
 import { useAuth } from '../../Contexts';
+import axios from 'axios';
 
 const NavBar = () => {
     const style = { textDecoration: 'none', fontWeight: 'bold', color: 'color: var(--PuertoRico)' };
     const { user, setUser } = useAuth();
 
     const handleLogout = async () => {
+        const tokenStr = localStorage.getItem('token')
+        
+        const data = {
+            headers: {
+                token: tokenStr
+            }
+        }
+        try {
+            const response = await axios.delete('http://localhost:3000/users/logout', data)
+        
+        } catch(err) {
+            console.log(err.message)
+        }
         localStorage.removeItem('token')
         await setUser('')
         // axios req to delete token
